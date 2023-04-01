a4a09a5cec5251a31ea62bea3b9536715f8f22c6
Luiza Bryn <luiza.bryn@grad.ufsc.br>
2023-04-01 15:48:38 -0300

    Ajustar dimensões das cartas e adicionar o label de mana

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class PlayerInterface:
         self.mainWindow["bg"] = "#DBE9F4"
         self.mainWindow.grid_columnconfigure(0, weight=1)
         self.mainWindow.grid_rowconfigure(0, weight=1)
-        self.mainWindow.grid_rowconfigure(1, weight=7)
+        self.mainWindow.grid_rowconfigure(1, weight=20)
         self.mainWindow.grid_rowconfigure(2, weight=3)
 
         self.statusBoard = Frame(self.mainWindow)
@@ -24,8 +24,14 @@ class PlayerInterface:
 
         self.lifePlayer1 = Label(self.statusBoard, text="Vida: X")
         self.lifePlayer1.grid(row=0, column=0)
+        self.manaPlayer1 = Label(
+            self.statusBoard, text="Mana: X", padx=0, pady=0)
+        self.manaPlayer1.grid(row=1, column=0)
         self.lifePlayer2 = Label(self.statusBoard, text="Vida: Y")
         self.lifePlayer2.grid(row=0, column=2)
+        self.lifePlayer2 = Label(
+            self.statusBoard, text="Mana: Y", padx=0, pady=0)
+        self.lifePlayer2.grid(row=1, column=2)
         self.roundCounter = Label(self.statusBoard, text="Round X")
         self.roundCounter.grid(row=0, column=1)
 
@@ -38,11 +44,16 @@ class PlayerInterface:
 
         self.cardBoard = Frame(self.barraTarefasFrame, bg="#56899F")
         self.cardBoard.grid(row=0, column=0, sticky="nsew")
+        self.cardBoard.grid_rowconfigure(0, weight=1)
+        self.cardBoard.grid_rowconfigure(1, weight=10)
+
         for i in range(5):
             self.cardBoard.grid_columnconfigure(i, weight=1)
-            carta = Button(self.cardBoard, text="Carta {}".format(i+1))
-            carta.grid(row=0, column=i)
-        self.pular_vez = Button(self.barraTarefasFrame, text="Passar vez", bg="#91DBBB", fg="black", font=("Arial", 14, "bold"),
+            carta = Button(self.cardBoard, text="Carta {}".format(
+                i+1), padx=50, pady=80)
+            carta.grid(row=1, column=i)
+
+        self.pular_vez = Button(self.barraTarefasFrame, text="Passar vez", bg="#91DBBB", fg="black", font=("Arial", 14),
                                 width=10, height=2, bd=5, relief="raised", activebackground="#254954", activeforeground="white")
         self.pular_vez.grid(row=0, column=1)
 
